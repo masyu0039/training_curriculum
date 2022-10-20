636e41d619d95ea73e23cdf710c6f60c0bb89b98
masyu0039 <114735314+masyu0039@users.noreply.github.com>
2022-10-20 11:14:03 +0900

    issue6
    色々試みましたが、曜日が表示されませんでした、、
    ＊issue４は解決しました

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,11 +15,12 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -29,12 +30,26 @@ class CalendarsController < ApplicationController
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
-    7.times do |x|
-      today_plans = []
-      plans.each do |plan|
-        today_plans.push(plan.plan) if plan.date == @todays_date + x
+7.times do |x|
+
+  today_plans = []
+  
+  plans.each do |plan|
+  
+  today_plans.push(plan.plan) if plan.date == @todays_date + x
+  
+  end
+  
+
+  wday_num = Date.today.wday + x
+  
+  if wday_num >= 7
+  
+  wday_num = wday_num -7
+  
       end
-      days = { month:(@todays_date + x).month, date:(@todays_date + x).day, plans:today_plans}
+
+      days = { month:(@todays_date + x).month, date:(@todays_date + x).day, plans:today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
 
